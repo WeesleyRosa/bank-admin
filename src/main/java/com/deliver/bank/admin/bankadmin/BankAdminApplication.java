1bb7e394d4d4c95b2ee6b87c8625887e5d7d8cad
Wesley Rosa <wesley.rosa@4all.local>
2020-03-09 13:10:05 -0300

    adding feign configuration in apllication class

--- src/main/java/com/deliver/bank/admin/bankadmin/BankAdminApplication.java
+++ src/main/java/com/deliver/bank/admin/bankadmin/BankAdminApplication.java
@@ -2,7 +2,9 @@ package com.deliver.bank.admin.bankadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BankAdminApplication {
 
