1a85f0aa85f5e7b453c74d088894285c4f9354cf
Wesley Rosa <wesley.rosa@4all.local>
2020-03-09 12:47:02 -0300

    first commit

---
+++ src/main/java/com/deliver/bank/admin/bankadmin/BankAdminApplication.java
@@ -0,0 +1,13 @@
+package com.deliver.bank.admin.bankadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliver/bank/admin/bankadmin/BankAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliver.bank.admin.bankadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
