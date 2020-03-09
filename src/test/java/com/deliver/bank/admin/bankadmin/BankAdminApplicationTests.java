4322c9f10e07de1e3d23659ed1f142e0f3e4c448
wesley.rosa <wesleypikemen@hotmail.com>
2020-03-09 13:29:17 -0300

    adding mysql and comment tests

--- src/test/java/com/deliver/bank/admin/bankadmin/BankAdminApplicationTests.java
+++ src/test/java/com/deliver/bank/admin/bankadmin/BankAdminApplicationTests.java
@@ -3,11 +3,11 @@ package com.deliver.bank.admin.bankadmin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BankAdminApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
